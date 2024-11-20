ae717bc17edb5ca7f5615db08e0abf44b03a86e6
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2024-11-21 03:09:33 +0500

    Add Setup

---
+++ react-breeze-api/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+     <section className="max-w-6xl mx-auto">
+     <h1 className="text-3xl font-bold underline">
+      Salam React+Laravel
+    </h1>
+     </section>
+    </>
+  )
+}
+
+export default App
